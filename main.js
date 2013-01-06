1188b4de355204d724a9e053a589c96ad222f51f
tao <tao@MacBook-Pro.local>
2013-01-06 20:28:30 +0900

    commit all

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+function showModal() {
+	var opt = "dialogwidth=400px; dialogheight=100px";
+	var md = window.showModalDialog("dialog.html", "test", opt);
+}
 function doFirst() {
 	var x = document.getElementById('canvas');
 	canvas = x.getContext('2d');
 	canvas.fillStyle="blue";
 	canvas.fillRect(10,10,100,200);
 	canvas.clearRect(20,20,50,90);
+	var enter = document.getElementById('enter');
+	enter.addEventListener('click', showModal, false);
 }
 window.addEventListener("load", doFirst, false);
